f707464981ac0bbab73b00ba4c1f905e399ce8f1
Hilal <hilalarsa@gmail.com>
2018-01-05 22:39:09 +0700

    Initial commit

---
+++ application/views/assets.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- INCLUDE BOOTSTRAP -->
+	<link href="<?php echo base_url() ?>bs/css/bootstrap.css" rel="stylesheet">
+	<link href="<?php echo base_url() ?>bs/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?php echo base_url() ?>bs/css/bootstrap-datepicker.css" rel="stylesheet">
+	<!--external css-->
+	<link href="<?php echo base_url() ?>bs/assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+	
+	<!-- INCLUDE DATA TABLES -->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>bs/css/jquery.dataTables.min.css">
+
+	
+</head>
+<body>
+	<!-- BOOTSTRAP JS -->
+	<script src="<?php echo base_url() ?>bs/assets/js/bootstrap.min.js"></script>
+
+	<script src="<?php echo base_url() ?>bs/css/jquery.dataTables.min.js"></script>
+	<script src="<?php echo base_url() ?>bs/css/dataTables.js"></script>
+	<script src="<?php echo base_url() ?>bs/assets/js/jquery.js"></script>
+</body>
+</html>
